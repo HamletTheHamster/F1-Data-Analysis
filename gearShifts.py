08ad5a679a6d53506c2b7916cd058c305bb73558
Joel N. Johnson <joel.johnson675@gmail.com>
2023-05-05 12:52:51 -0700

    gearChanges

---
+++ gearShifts.py
@@ -0,0 +1,54 @@
+import fastf1
+
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib import cm
+import numpy as np
+import datetime
+import os
+import sys
+
+timestamp = datetime.datetime.now()
+
+if len(sys.argv) > 0:
+    note = sys.argv[1]
+
+save = "Plots/" + timestamp.strftime("%Y-%b-%d") + "/" + timestamp.strftime("%X") + ": " + note + "/"
+if not os.path.exists(save):
+  os.makedirs(save)
+
+
+session = fastf1.get_session(2023, 'Baku', 'Q')
+session.load()
+
+lap = session.laps.pick_fastest()
+tel = lap.get_telemetry()
+
+x = np.array(tel['X'].values)
+y = np.array(tel['Y'].values)
+
+points = np.array([x, y]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+gear = tel['nGear'].to_numpy().astype(float)
+
+cmap = cm.get_cmap('Paired')
+lc_comp = LineCollection(segments, norm=plt.Normalize(1, cmap.N+1), cmap=cmap)
+lc_comp.set_array(gear)
+lc_comp.set_linewidth(4)
+
+plt.gca().add_collection(lc_comp)
+plt.axis('equal')
+plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+
+title = "Fastest Lap Gear Shift Visualization"
+plt.suptitle(
+    f"{title}\n"
+    f"{lap['Driver']} - {session.event['EventName']} {session.event.year}"
+)
+
+cbar = plt.colorbar(mappable=lc_comp, label="Gear", boundaries=np.arange(1, 10))
+cbar.set_ticks(np.arange(1.5, 9.5))
+cbar.set_ticklabels(np.arange(1, 9))
+
+plt.savefig(save + title + ".pdf", format="pdf")
+plt.savefig(save + title + ".png", format="png")
